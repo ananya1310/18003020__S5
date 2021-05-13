c6b96d7f208301f5a7dffc4a0c1825d394ed9cb8
DELZL <DELZL@DESKTOP-4FR2AG0>
2021-05-13 06:57:28 +0530

    Arrays using Java

---
+++ Arrays/src/TwentyEight.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class TwentyEight {
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.print("Enter number of elements ");
+	    int n = sc.nextInt();
+	    int[] a =new int[n];
+	    int i,j;
+	    for(i=0;i<n;i++) 
+	    {
+	      a[i]=sc.nextInt();
+	    }
+	  }
+
+}
